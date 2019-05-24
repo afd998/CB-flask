620673266e8d10b8ea4232ae1a92e0b1ce14c658
Atticus Deutsch <afd998@gmail.com>
2019-05-23 19:57:56 -0700

    added personal content

--- flask_heroku_example/app.py
+++ flask_heroku_example/app.py
@@ -1,9 +1,12 @@
 import os
 import psycopg2
 from flask import Flask, render_template, g
+import test
+from flask import Flask, render_template, jsonify, request, session, redirect
+import warnings, csv
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="htmltemplates")
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
 
 
@@ -20,4 +23,48 @@ def before_request():
 def index():
     cur = g.db_conn.cursor()
     cur.execute("SELECT * FROM country;")
-    return render_template('index.html', countries=cur.fetchall())
+
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return "<h1>About page</h1>"
+
+@app.route("/update",methods= ['GET'])
+def update():
+
+    final_string = str(main())
+    return jsonify(keke=final_string)
+
+@app.route("/login", methods= ['POST'])
+def login():
+    username= request.form['username']
+    password= request.form['password']
+    if username=="username" and password=="password":
+        session['username'] = 'admin'
+        return redirect('/dashboard')
+    else:
+        return render_template('index.html', message="Wrong username/password!")
+
+
+
+@app.route("/dashboard", methods= ['GET'])
+def dshboard():
+    if 'username' in session:
+        return render_template('dashboard.html')
+    else:
+        return redirect('/')
+
+
+
+@app.route("/logout", methods= ['GET'])
+def logout():
+    session.pop('username', None)
+    return redirect('/')
+
+
+
+
+#if __name__ == '__main__':
+
+    #app.run(debug=True)
